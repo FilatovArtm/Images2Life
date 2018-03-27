675aed8ea12695aff7be071f079a11ceb750f036
Artem Filatov <filatovartm@gmail.com>
2018-03-27 10:44:23 +0300

    simultanious training of image and video

--- experiment/spatial_utils.py
+++ experiment/spatial_utils.py
@@ -50,6 +50,21 @@ class BatchGenerator:
         else:
             return self.maps_generator_(len(self.target_), len(self.target_) + 64, self.k, self.r)
 
+class BatchGeneratorVideoAndImage:
+    def __init__(self, target, maps_generator_video, maps_generator_image, k, r):
+        self.target_ = target
+        self.maps_generator_video_ = maps_generator_video
+        self.maps_generator_image_ = maps_generator_image
+        self.k = k
+        self.r = r
+        self.T = len(target)
+
+    def __call__(self, mode='train'):
+        if mode == 'train':
+            return torch.cat([self.maps_generator_video_(0, len(self.target_) - 1, self.k, self.r), 
+                              self.maps_generator_image_(0, 1, self.k, self.r)]), self.target_
+        else:
+            return self.maps_generator_image_(0, 64, self.k, self.r)
 
 
 class PerceptualLoss:
