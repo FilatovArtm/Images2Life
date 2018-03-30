3e9f0fa6f60e1ad83885902e511f8295370f5e62
Artem Filatov <filatovartm@gmail.com>
2018-03-30 17:10:27 +0300

    time vector generator

--- experiment/spatial_utils.py
+++ experiment/spatial_utils.py
@@ -34,6 +34,21 @@ class SpatialMapsGenerator:
 
         return result
 
+class SpatialVectorGenerator:
+    def __init__(self, time_size, picture_size, noise_level=0.1):
+        self.variables_ = {"picture": None, "time_gamma": None, "time_delta": None}
+        self.variables_["picture"] = numpyToVar(np.random.normal(
+                0, noise_level, picture_size), requires_grad=True)
+        self.variables_["time_gamma"]  = numpyToVar(np.random.normal(
+                0, noise_level, time_size), requires_grad=True)
+        self.variables_["time_delta"]  = numpyToVar(np.random.normal(
+                0, noise_level, time_size), requires_grad=True)
+
+    def __call__(self, start_T, end_T, k=0, r=0):
+        res = torch.ger(torch.arange(start_T, end_T), self.variables_["time_gamma"]) + self.variables_["time_delta"]
+        pic = self.variables_["picture"].expand(end_T - start_T, len(self.variables_["picture"]))
+        return torch.cat([pic, res])
+
 
 class BatchGenerator:
 
