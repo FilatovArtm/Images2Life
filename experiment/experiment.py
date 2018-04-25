a592dd856e94ba2f43d8fa64ee0be163054f1056
students <aafilatov@almaren.yandex.ru>
2018-04-25 11:51:53 +0300

    last modifications

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -4,6 +4,79 @@ import json
 import os
 import numpy as np
 
+class Experiment2:
+
+    def __init__(self, config, optimize_parameters, data_loader, net, loss, plotter=plotter):
+        self.config_ = config
+        self.optimize_parameters_ = optimize_parameters
+        self.data_loader_ = data_loader
+        self.net_ = net
+        self.loss_ = loss
+        self.plotter_ = plotter
+
+    def run(self):
+        iterator = iter(self.data_loader_)
+        i = 0
+        
+
+        def closure():
+            nonlocal iterator
+            nonlocal i
+            try:
+                X, Y = next(iterator)
+            except:
+                iterator = iter(self.data_loader_)
+                X, Y = next(iterator)
+                
+            X, Y = X.cuda(), Y.cuda()
+            Y_hat = self.net_(X)
+
+            total_loss = self.loss_(Y, Y_hat)
+            total_loss.backward()
+
+            print('Iteration %05d    Loss %f' %
+                  (i, total_loss.data[0]), '\r', end='')
+            if self.config_["PLOT"] and i % self.config_["show_every"] == 0:
+                self.plotter_(Y_hat)
+                
+            i += 1
+            return total_loss
+
+        optimize(self.config_["optimizer"], self.optimize_parameters_,
+                 closure, self.config_["lr"], self.config_["num_iter"])
+
+    def predict_video(self, test_generator):
+        result = []
+
+        for i in range(len(test_generator)):
+            X = test_generator[i].cuda()
+            Y_hat = self.net_(X)
+            result.append(prepareWriting(Y_hat))
+
+        return np.concatenate(result)
+
+    def save_result(self, test_generator, train_generator):
+        video_predict = self.predict_video(test_generator)
+        file_name = time.strftime("%d_%b_%Y:%H:%M:%S", time.gmtime())
+        path = 'experiment_results/{}'.format(file_name)
+        self.path_ = path
+        os.makedirs(path)
+
+        write_video(path + "/predict.mp4", video_predict)
+
+        video_fit = self.predict_video(train_generator)
+        write_video(path + "/fit.mp4", video_fit)
+
+        #video_target = prepareWriting(self.dat.target_)
+        #write_video(path + "/target.mp4", video_target)
+
+        with open(path + "/config.json", "w") as f:
+            json.dump(self.config_, f)
+
+
+
+    
+
 class Experiment:
 
     def __init__(self, config, optimize_parameters, batch_generator, net, loss, plotter=plotter):
--- models/gram_and_learn.py
+++ models/gram_and_learn.py
@@ -6,7 +6,22 @@ from utils.common_utils import generateSyntheticTexture, \
                          generateSyntheticImage, \
                          preprocessTarget, \
                          numpyToVar
+import skvideo
 
+def loss(y_hat, y):
+    result = 0
+    for i in range(len(y)):
+        if i % 2 == 0:
+            gram_y = y[i] @ torch.transpose(y[i], 2, 1)
+            gram_yhat = y_hat[i] @ torch.transpose(y_hat[i], 2, 1)
+            result += torch.sum((gram_y - gram_yhat) ** 2) / (gram_y.shape[1] ** 2) / 3
+        else:
+            result += torch.sum((y[i] - y_hat[i]) ** 2) / (y.shape[1] ** 2) / 3
+            
+    return result / len(y)
+
+
+            
 
 class TimeMapsGenerator:
 
@@ -27,11 +42,18 @@ class TimeMapsGenerator:
 
 
 class GramAndImages(torch.utils.data.TensorDataset):
-    def __init__(self, video_length, video_seed, maps_number, size):
+    def __init__(self, video_length, maps_number, size, video_seed=0, video_path=None):
+        
         self.size_ = size
         self.video_length_ = video_length
-        self.video_ = generateSyntheticTexture(random=True, seed=video_seed)
-
+        self.video_path_ = video_path
+        
+        if video_path == None:
+            self.video_ = generateSyntheticTexture(random=True, seed=video_seed)
+        else:
+            self.full_video_ = skvideo.io.vread(fname=video_path)
+            self.video_ = skvideo.io.vread(fname=video_path)[:, :size, :size]
+            
         # resize and transpose
         self.video_ = preprocessTarget(self.video_, video_length, size)
         self.maps_generator_ = TimeMapsGenerator(maps_number)
@@ -40,7 +62,12 @@ class GramAndImages(torch.utils.data.TensorDataset):
         '''
         One unit contains one image of video and one picture
         '''
-        image = generateSyntheticImage(index)[0]
+        if self.video_path_ == None:
+            image = generateSyntheticImage(index)[0]
+        else:
+            np.random.seed(index)
+            coord = np.random.choice(len(self.full_video_[0]) - self.size_, size=2)
+            image = self.full_video_[index, coord[0]:coord[0] + self.size_, coord[1]:coord[1] + self.size_]
         image = np.transpose(image, [2, 0, 1])
         image = numpyToVar(
             resize(image, output_shape=(3, self.size_, self.size_), mode='constant'))
@@ -48,12 +75,43 @@ class GramAndImages(torch.utils.data.TensorDataset):
 
         video_frame = self.video_[index]
 
+        np.random.seed(index)
+        time_stamp = np.random.choice(self.video_length_)
         input_im = torch.cat(
-            [image, self.maps_generator_(self.size_, time_stamp=0)])
+            [image, self.maps_generator_(self.size_, time_stamp=time_stamp)])
         input_vid = torch.cat(
-            [video_frame, self.maps_generator_(self.size_, time_stamp=index)])
+            [self.video_[0], self.maps_generator_(self.size_, time_stamp=index)])
 
         return torch.cat([input_im[None], input_vid[None]]), torch.cat([image[None], video_frame[None]])
 
     def __len__(self):
         return self.video_length_
+    
+    
+
+    
+class GramAndImagesTest(torch.utils.data.TensorDataset):
+    def __init__(self, video_length, video_seed, maps_number, size, maps_generator, origin_frame=None):
+        
+        self.size_ = size
+        self.video_length_ = video_length
+
+        # resize and transpose
+        if origin_frame is None:
+            self.image_ = generateSyntheticImage(video_seed + 1000)
+        else:
+            self.image_ = origin_frame
+            
+        if self.image_.shape[1] != 3:
+            self.image_ = preprocessTarget(self.image_, 1, size)
+        self.maps_generator_ = maps_generator
+
+    def __getitem__(self, index):
+
+        input_im = torch.cat([self.image_, self.maps_generator_(self.size_, time_stamp=index)[None]], dim=1)
+
+        return input_im
+
+    def __len__(self):
+        return self.video_length_
+
